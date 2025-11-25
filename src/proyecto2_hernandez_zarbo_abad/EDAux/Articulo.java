4a9d08080349675cd50acaf6f4d07f2ad286daa9
Victoria Hernandez <>
2025-11-24 22:45:10 -0400

    Clase de investigacion

---
+++ src/proyecto2_hernandez_zarbo_abad/EDAux/Articulo.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2_hernandez_zarbo_abad.EDAux;
+
+/**
+ *
+ * @author victoria
+ */
+
+public class Articulo {
+    private String titulo;
+    private String[] autores;
+    private String resumen;
+    private String[] palabrasClaves;
+
+    public Articulo(String titulo, String[] autores, String resumen, String[] palabrasClaves) {
+        this.titulo = titulo;
+        this.autores = autores;
+        this.resumen = resumen;
+        this.palabrasClaves = palabrasClaves;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String[] getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String[] autores) {
+        this.autores = autores;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public String[] getPalabrasClaves() {
+        return palabrasClaves;
+    }
+
+    public void setPalabrasClaves(String[] palabrasClaves) {
+        this.palabrasClaves = palabrasClaves;
+    }
+}
